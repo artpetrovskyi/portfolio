62e7040b8498dbf048ac2c0b9df9a2ebde123607
Artem Petrovskyi <artpetrovskyi@gmail.com>
2025-07-13 17:22:17 +0300

    add media

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <div className="flex min-h-svh flex-col items-center justify-center">
-        <Button>Click me</Button>
+        <Button>Click me!!!</Button>
         <ModeToggle />
       </div>
     </ThemeProvider>
