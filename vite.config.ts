469aa80b363eb727afba060733788c65e1c0d68f
Artem Petrovskyi <artpetrovskyi@gmail.com>
2025-08-05 16:14:58 +0300

    add a custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
@@ -11,5 +10,5 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "/portfolio/",
+  // base: "/portfolio/", // for gh-pages. Remove to use a custom domain
 });
